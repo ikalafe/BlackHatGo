fc4b114b8cffb14f5fc41d3ba88b50331b1f30d5
ikalafe <imkalafe@gmail.com>
2025-07-11 19:12:03 -0400

    feat: add hostname-based reverse proxy using Gorilla Mux and httputil

---
+++ ch4/multiplexer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	hostProxy = make(map[string]string)
+	proxies   = make(map[string]*httputil.ReverseProxy)
+)
+
+func init() {
+	hostProxy["attacker1.com"] = "http://192.168.115.195:8081"
+	hostProxy["attacker2.com"] = "http://192.168.115.195:8082"
+
+	for k, v := range hostProxy {
+		remote, err := url.Parse(v)
+		if err != nil {
+			log.Fatal("Unable to parse proxy target")
+		}
+		proxies[k] = httputil.NewSingleHostReverseProxy(remote)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	for host, proxy := range proxies {
+		r.Host(host).Handler(proxy)
+	}
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
